1271796be345076ec3c41d6f146519a08cb59763
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:34:57 -0500

    Update delete_payment.js

--- delete_payment.js
+++ delete_payment.js
@@ -1,23 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-
-    if(isset($_GET['delete_payment'])){
-        
-        delete_payment_id = $_GET['delete_payment'];
-        
-        delete_payment = "delete from payments where payment_id='delete_payment_id'";
-        
-        run_delete = mysqli_query(con,delete_payment);
-        
+            
         if(run_delete){
             
-            console.log( "<script>alert('One of Your Payment Has Been Deleted')</script>");
+            <script>alert('One of Your Payment Has Been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_payments %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_payments %>);
+            </script>
             
         }
         
