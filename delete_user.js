c817acb7a2e4d358ecc567171e104359e2516dae
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:44:23 -0500

    Update delete_user.js

--- delete_user.js
+++ delete_user.js
@@ -1,22 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-    if(isset($_GET['delete_user'])){
-        
-        delete_user_id = $_GET['delete_user'];
-        
-        delete_user = "delete from admins where admin_id='delete_user_id'";
-        
-        run_delete = mysqli_query(con,delete_user);
         
         if(run_delete){
             
-            console.log("<script>alert('One of your Admins User has been Deleted')</script>");
+            <script>alert('One of your Admins User has been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_users %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_users %>);
+            </script>
             
         }
         
