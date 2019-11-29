42a77c912818eefa069ee4832aee0e621cb659aa
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:32:51 -0500

    Update delete_p_cat.js

--- delete_p_cat.js
+++ delete_p_cat.js
@@ -1,22 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-    if(isset($_GET['delete_p_cat'])){
-        
-        delete_p_cat_id = $_GET['delete_p_cat'];
-        
-        delete_p_cat = "delete from product_categories where p_cat_id='delete_p_cat_id'";
-        
-        run_delete = mysqli_query(con,delete_p_cat);
         
         if(run_delete){
             
-            console.log("<script>alert('One of Your Product Has Been Deleted')</script>");
+            <script>alert('One of Your Product Has Been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_p_cats %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_p_cats %>);
+            </script>
             
         }
         
