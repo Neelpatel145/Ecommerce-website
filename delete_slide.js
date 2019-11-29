32e5406db2c4d3ef7f393314476cc22d05cb6eef
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:40:01 -0500

    Update delete_slide.js

--- delete_slide.js
+++ delete_slide.js
@@ -1,24 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-
-
-    if(isset($_GET['delete_slide'])){
-        
-        delete_slide_id = $_GET['delete_slide'];
-        
-        delete_slide = "delete from slider where slide_id='delete_slide_id'";
-        
-        run_delete = mysqli_query(con,delete_slide);
         
         if(run_delete){
             
-            console.log("<script>alert('One of Your Slide Has Been Deleted')</script>");
+            <script>alert('One of Your Slide Has Been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_slides %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_slides %>);
+            </script>
             
         }
         
