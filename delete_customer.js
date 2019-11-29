f929df7f9b74384c1dac5f60e96df555f52d0ec1
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 06:08:53 -0500

    Create delete_customer.js

---
+++ delete_customer.js
@@ -0,0 +1,29 @@
+if(!isset($_SESSION['admin_email'])){
+        
+        console.log("<script>window.open('login.ejs','_self')</script>");
+        
+    }else{
+
+
+
+    if(isset($_GET['delete_coupon'])){
+        
+        delete_coupon_id = $_GET['delete_coupon'];
+        
+        delete_coupon = "delete from coupons where coupon_id='delete_coupon_id'";
+        
+        run_delete = mysqli_query(con,delete_coupon);
+        
+        if(run_delete){
+            
+            console.log("<script>alert('One of Your Coupons Has Been Deleted')</script>");
+            
+            console.log("<script>window.open('/index.ejs/<%= view_coupons %>','_self')</script>");
+            
+        }
+        
+    }
+
+
+
+} 
