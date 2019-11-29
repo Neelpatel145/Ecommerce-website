ae377e1f8691ee0ade7031e21a1e02bbd8807787
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:24:30 -0500

    Update delete_cat.js

--- delete_cat.js
+++ delete_cat.js
@@ -1,22 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-    if(isset($_GET['delete_cat'])){
-        
-        delete_cat_id = $_GET['delete_cat'];
-        
-        delete_cat = "delete from categories where cat_id='delete_cat_id'";
-        
-        run_delete = mysqli_query(con,delete_cat);
-        
+            
         if(run_delete){
             
-            console.log("<script>alert('One of Your Category Has Been Deleted')</script>");
+            <script>alert('One of Your Category Has Been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_cats %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_cats %>);
+           </script>
             
         }
         
