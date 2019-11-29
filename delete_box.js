5155ecd93c7d7278521794802bba267a87cd9baa
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:22:41 -0500

    Update delete_box.js

--- delete_box.js
+++ delete_box.js
@@ -1,22 +1,12 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.php','_self')</script>");
+<script> window.open(/login/'); </script>
         
     }else{
-
-    if(isset($_GET['delete_box'])){
-        
-        delete_box_id = $_GET['delete_box'];
-        
-        delete_box = "delete from boxes_section where box_id='delete_box_id'";
-        
-        run_delete_box = mysqli_query(con,delete_box);
         
         if(run_delete_box){
             
-            console.log("<script>alert('One of Your Box Section Has Been Deleted')</script>");
+            <script>alert('One of Your Box Section Has Been Deleted');</script>"
             
-            console.log("<script>window.open('index.ejs?view_boxes','_self')</script>");
+            <script>window.open(/index/<%= view_boxes %>);</script>
             
         }
         
