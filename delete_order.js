77f2796153a3103351ecf4bd95eb76d0b7c9cf98
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:31:24 -0500

    Update delete_order.js

--- delete_order.js
+++ delete_order.js
@@ -1,22 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-    if(isset($_GET['delete_order'])){
-        
-        delete_id = $_GET['delete_order'];
-        
-        delete_order = "delete from pending_orders where order_id='delete_id'";
-        
-        run_delete = mysqli_query(con,delete_order);
-        
+            
         if(run_delete){
             
-            console.log("<script>alert('One of your costumer order has been Deleted')</script>");
+            <script>alert('One of your costumer order has been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_orders %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_orders %>);
+            </script>
             
         }
         
