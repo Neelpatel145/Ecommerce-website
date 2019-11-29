c427aeb1abf56bb4d8ca7e12a63c2e5363055e9c
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:29:45 -0500

    Update delete_manufacturer.js

--- delete_manufacturer.js
+++ delete_manufacturer.js
@@ -1,22 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-    if(isset($_GET['delete_manufacturer'])){
-        
-        delete_id = $_GET['delete_manufacturer'];
-        
-        delete_manufacturer = "delete from manufacturers where manufacturer_id='delete_id'";
-        
-        run_delete = mysqli_query(con,delete_manufacturer);
         
         if(run_delete){
             
-            console.log("<script>alert('One of your manufacturer has been Deleted')</script>");
+            <script>alert('One of your manufacturer has been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_manufacturers %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_manufacturers %>);
+            </script>
             
         }
         
