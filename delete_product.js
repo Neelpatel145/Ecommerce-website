ae7acaa44f6633b3a5827deed4f08efa16153993
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 18:37:40 -0500

    Update delete_product.js

--- delete_product.js
+++ delete_product.js
@@ -1,23 +1,16 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-
-    if(isset($_GET['delete_product'])){
-        
-        delete_id = $_GET['delete_product'];
-        
-        delete_pro = "delete from products where product_id='delete_id'";
-        
-        run_delete = mysqli_query(con,delete_pro);
         
         if(run_delete){
             
-            console.log("<script>alert('One of your product has been Deleted')</script>");
+            <script>alert('One of your product has been Deleted')</script>
             
-            console.log("<script>window.open('/index.ejs/<%= view_products %>','_self')</script>");
+            <script>
+                    window.open(/index/<%= view_products %>);
+            </script>
             
         }
         
