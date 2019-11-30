42b0ad4e3c0e04a4cb87346f70538c60230f6453
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2019-11-29 19:34:32 -0500

    Update edit_manufacturer.js

--- edit_manufacturer.js
+++ edit_manufacturer.js
@@ -1,170 +1,126 @@
-if(!isset($_SESSION['admin_email'])){
-        
-        console.log("<script>window.open('login.ejs','_self')</script>");
+<script>
+        window.open(/login/);
+</script>
         
     }else{
-
-        if(isset($_GET['edit_manufacturer'])){
-
-            edit_manufacturer = $_GET['edit_manufacturer'];
-            get_manufacturer = "select * from manufacturers where manufacturer_id='edit_manufacturer'";
-            run_manufacturer = mysqli_query(con,get_manufacturer);
-            row_manufacturer = mysqli_fetch_array(run_manufacturer);
-
-            m_id = row_manufacturer['manufacturer_id'];
-            m_title = row_manufacturer['manufacturer_title'];
-            m_top = row_manufacturer['manufacturer_top'];
-            m_image = row_manufacturer['manufacturer_image'];
-
-        }
-
-<div class="row"><!-- row 1 begin -->
-    <div class="col-lg-12"><!-- col-lg-12 begin -->
-        <ol class="breadcrumb"><!-- breadcrumb begin -->
+            
+<div class="row">
+    <div class="col-lg-12">
+        <ol class="breadcrumb">
             <li>
                 
                 <i class="fa fa-dashboard"></i> Dashboard / Update Manufacturer
                 
             </li>
-        </ol><!-- breadcrumb finish -->
-    </div><!-- col-lg-12 finish -->
-</div><!-- row 1 finish -->
+        </ol>
+    </div>
+</div>
 
-<div class="row"><!-- row 2 begin -->
-    <div class="col-lg-12"><!-- col-lg-12 begin -->
-        <div class="panel panel-default"><!-- panel panel-default begin -->
-            <div class="panel-heading"><!-- panel-heading begin -->
-                <h3 class="panel-title"><!-- panel-title begin -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">
                 
                     <i class="fa fa-money fa-fw"></i> Update Manufacturer
                 
-                </h3><!-- panel-title finish -->
-            </div><!-- panel-heading finish -->
+                </h3>
+            </div>
             
-            <div class="panel-body"><!-- panel-body begin -->
-                <form action="" class="form-horizontal" method="post" enctype="multipart/form-data"><!-- form-horizontal begin -->
-                    <div class="form-group"><!-- form-group 1 begin -->
+            <div class="panel-body">
+                <form action="" class="form-horizontal" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
                     
-                        <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
+                        <label for="" class="control-label col-md-3">
                         
                         Manufacturer Name 
                         
-                        </label><!-- control-label col-md-3 finish --> 
+                        </label>
                         
-                        <div class="col-md-6"><!-- col-md-6 begin -->
+                        <div class="col-md-6">
                         
-                            <input name="manufacturer_name" type="text" class="form-control" value="console.log(m_title); ">
+                            <input name="manufacturer_name" type="text" class="form-control" value="m_title">
                         
-                        </div><!-- col-md-6 finish -->
+                        </div>
                     
-                    </div><!-- form-group 1 finish -->
-                    <div class="form-group"><!-- form-group 2 begin -->
+                    </div>
+                    <div class="form-group">
                     
-                        <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
+                        <label for="" class="control-label col-md-3"> 
                         
                             Choose As Top Manufacturer
                         
-                        </label><!-- control-label col-md-3 finish --> 
+                        </label>
                         
-                        <div class="col-md-6"><!-- col-md-6 begin -->
+                        <div class="col-md-6">
                         
-                            <input name="manufacturer_top" type="radio" value="yes"
-
-                                if(m_top=='no'){}else{echo "checked='checked'";}
-                                
-                            
-                            >
+                            <input name="manufacturer_top" type="radio" value="yes">
                             <label>Yes</label>
                         
-                            <input name="manufacturer_top" type="radio" value="no"
-                            
-                                if(m_top=='no'){echo "checked='checked'";}
-                            
-                            >
+                            <input name="manufacturer_top" type="radio" value="no">
                             <label>No</label>
                         
-                        </div><!-- col-md-6 finish -->
+                        </div>
                     
-                    </div><!-- form-group 2 finish -->
-                    <div class="form-group"><!-- form-group 3 begin -->
+                    </div>
+                    <div class="form-group">
                     
-                        <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --> 
+                        <label for="" class="control-label col-md-3"> 
                         
                         Manufacturer Image
                         
-                        </label><!-- control-label col-md-3 finish --> 
+                        </label>
                         
-                        <div class="col-md-6"><!-- col-md-6 begin -->
+                        <div class="col-md-6">
                         
                             <input type="file" name="manufacturer_image" class="form-control">
                           
                           <br>
                           
-                          <img width="70" height="70" src="other_images/console.log(m_image);" alt="console.log(m_image);">
+                          <img width="70" height="70" src="/other_images/<%= m_image %>" alt="m_image">
                         
-                        </div><!-- col-md-6 finish -->
+                        </div>
                     
-                    </div><!-- form-group 3 finish -->
-                    <div class="form-group"><!-- form-group 4 begin -->
+                    </div>
+                    <div class="form-group">
                     
-                        <label for="" class="control-label col-md-3"><!-- control-label col-md-3 begin --></label><!-- control-label col-md-3 finish --> 
+                        <label for="" class="control-label col-md-3"></label>
                         
-                        <div class="col-md-6"><!-- col-md-6 begin -->
+                        <div class="col-md-6">
                         
                             <input type="submit" name="update" value="Update Manufacturer" class="btn btn-primary form-control">
                         
-                        </div><!-- col-md-6 finish -->
+                        </div>
                     
-                    </div><!-- form-group 4 finish -->
-                </form><!-- form-horizontal finish -->
-            </div><!-- panel-body finish -->
+                    </div>
+                </form>
+            </div>
             
-        </div><!-- panel panel-default finish -->
-    </div><!-- col-lg-12 finish -->
-</div><!-- row 2 finish -->
-
-if(isset($_POST['update'])){
-        
-        manufacturer_name = $_POST['manufacturer_name'];
-        
-        manufacturer_top = $_POST['manufacturer_top'];
-
-        if(is_uploaded_file($_FILES['manufacturer_image']['tmp_name'])){
-        
-            manufacturer_image = $_FILES['manufacturer_image']['name'];
-            
-            temp_name = $_FILES['manufacturer_image']['tmp_name'];
-                
-            move_uploaded_file(temp_name,"other_images/manufacturer_image");
-            
-            update_manufacturer = "update manufacturers set manufacturer_title='manufacturer_name',manufacturer_top='manufacturer_top',manufacturer_image='manufacturer_image'where manufacturer_id='m_id'" ;
-            
-            run_manufacturer = mysqli_query(con,update_manufacturer);
+        </div>
+    </div>
+</div>
 
             if(run_manufacturer){
             
-                console.log("<script>alert('Your manufacturer has been updated')</script>");
+                <script>alert('Your manufacturer has been updated')</script>
                 
-                console.log("<script>window.open('/index.ejs/<%= view_manufacturers %>','_self')</script>");
+                <script>
+                        window.open(/index/<%= view_manufacturers %>);
+                </script>
 
             }
 
         }else{
-            
-            update_manufacturer = "update manufacturers set manufacturer_title='manufacturer_name',manufacturer_top='manufacturer_top' where manufacturer_id='m_id'" ;
-            
-            run_manufacturer = mysqli_query(con,update_manufacturer);
 
             if(run_manufacturer){
             
-                console.log("<script>alert('Your manufacturer has been updated')</script>");
+                <script>alert('Your manufacturer has been updated')</script>
                 
-                console.log("<script>window.open('/index.ejs/<%= view_manufacturers %>','_self')</script>");
+                <script>
+                        window.open(/index/<%= view_manufacturers %>);
+                </script>
 
             }
 
         }
         
-    }
-
-}
